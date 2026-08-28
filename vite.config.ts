b7369ef092b17b60f8edcf0efd00578a4e741224
Omuzoid <pekkacoc1323@gmail.com>
2026-08-28 12:29:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // GitHub Pages repository path
+  base: '/sih-prototype/',
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
